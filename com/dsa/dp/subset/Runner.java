19c469d5b69c95ca2406ac3d47a348ee217db408
Prerna <pregandhi@paypal.com>
2022-05-17 19:07:48 +0530

    Dynamic Programming : Subset Problem recursive solution

--- com/dsa/dp/subset/Runner.java
+++ com/dsa/dp/subset/Runner.java
@@ -1,13 +1,38 @@
 package com.dsa.dp.subset;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Runner {
     public static void main(String[] args) {
-        // int [] values = {1, 7, 4, 3, 6, 5, 9, 11};
-        // int sum = 43;
-        int [] values = {3,4,5};
-        int sum = 10;
-        SubsetProblem subsetProblem = new SubsetProblem(sum, values );
-        subsetProblem.solve();
+        int[] values = {1, 7, 4, 3, 6, 5, 9, 11};
+        int sum = 43;
+        int index = 0;
+        // int [] values = {3,4,5};
+        // int sum = 10;
+        //index = values.length - 1;
+        SubsetProblem subsetProblem;
+        //subsetProblem = new SubsetProblem(sum, values);
+        //subsetProblem.solve();
+        //subsetProblem.showResult();
+
+        //values = new int[]{0, 1, 7, 4, 3, 6, 5, 9, 11};
+        //sum = 43;
+        //values = new int[]{0, 3, 4, 5};
+        //sum = 10;
+        //index = values.length - 1;
+        //subsetProblem = new SubsetProblem(sum, values);
+        //System.out.println(subsetProblem.solveRecursive(sum, index));
+
+        values = new int[]{0, 1, 7, 4, 3, 6, 5, 9, 11};
+        sum = 43;
+        //values = new int[]{0, 3, 4, 5};
+        //sum = 10;
+        values = new int[]{0, 3, 4, 7};
+        sum = 10;
+        index = values.length - 1;
+        subsetProblem = new SubsetProblem(sum, values);
+        System.out.println(subsetProblem.solveMemoizedRecursive(sum, index, new HashMap<>()));
         subsetProblem.showResult();
     }
 }
--- com/dsa/dp/subset/SubsetProblem.java
+++ com/dsa/dp/subset/SubsetProblem.java
@@ -1,14 +1,18 @@
 package com.dsa.dp.subset;
 
+import java.util.Map;
+
 public class SubsetProblem {
     private int sum;
-    private int [] values;
-    private boolean [][] dpTable;
+    private int[] values;
+    private boolean[][] dpTable;
 
-    public SubsetProblem(int sum, int [] values){
+    public SubsetProblem(int sum, int[] values) {
         this.sum = sum;
         this.values = values;
         this.dpTable = new boolean[values.length + 1][sum + 1];
+        for (int i = 0; i < values.length + 1; i++)
+            dpTable[i][0] = true;
     }
 
     public void solve() {
@@ -16,42 +20,87 @@ public class SubsetProblem {
         // so no need to set it
         // first row has false values
         // first column has true values
-        for(int i = 0; i < values.length + 1;i++)
+        for (int i = 0; i < values.length + 1; i++)
             dpTable[i][0] = true;
 
-        for(int rowIndex = 1; rowIndex < values.length + 1; rowIndex++) {
-            for(int colIndex = 1; colIndex < sum + 1; colIndex++) {
+        for (int rowIndex = 1; rowIndex < values.length + 1; rowIndex++) {
+            for (int colIndex = 1; colIndex < sum + 1; colIndex++) {
                 // if colIndex is small , then we copy value above
-                if(colIndex < values[rowIndex - 1]) {
-                    dpTable[rowIndex][colIndex] = dpTable [rowIndex -1][colIndex];
+                if (colIndex < values[rowIndex - 1]) {
+                    dpTable[rowIndex][colIndex] = dpTable[rowIndex - 1][colIndex];
                 } else {
-                    if(dpTable[rowIndex - 1][colIndex]) {
+                    if (dpTable[rowIndex - 1][colIndex]) {
                         // we don't include that item
                         dpTable[rowIndex][colIndex] = dpTable[rowIndex - 1][colIndex];
                     } else {
                         dpTable[rowIndex][colIndex] = dpTable[rowIndex - 1][colIndex - values[rowIndex - 1]];
                     }
                 }
-
             }
         }
     }
 
+    public boolean solveRecursive(int sum, int i) {
+        if (sum == 0) {
+            return true;
+        }
+        if (i == 0) {
+            return false;
+        }
+        if (sum < values[i]) {
+            return solveRecursive(sum, i - 1);
+        } else {
+            return solveRecursive(sum - values[i], i - 1) || solveRecursive(sum, i - 1);
+        }
+    }
+
+    // Memoized solution not working yet
+    public boolean solveMemoizedRecursive(int sum, int i, Map<String, Boolean> lookup) {
+        boolean result;
+        String key = i + "|" + sum;
+        if (sum == 0) {
+            return true;
+        } else if (i < 0 || sum < 0) {
+            return false;
+        } else if (sum < values[i]) {
+            return dpTable[i][sum] = solveMemoizedRecursive(sum, i - 1, lookup);
+        } else {
+            if (
+                    solveMemoizedRecursive(sum - values[i], i - 1, lookup) ||
+                            solveMemoizedRecursive(sum, i - 1, lookup)) {
+                return dpTable[i][sum] = true;
+            } else {
+                return dpTable[i][sum] = false;
+
+            }
+        }
+        //  if (!lookup.containsKey(key)) {
+        //     result = solveMemoizedRecursive(sum - values[i], i - 1, lookup) || solveMemoizedRecursive(sum, i - 1, lookup);
+        //   lookup.put(key, result);
+        //    dpTable[i][sum] = result;
+        //}
+        //int k1 = Integer.parseInt(key.split("\\|")[0]);
+        //int k2 = Integer.parseInt(key.split("\\|")[1]);
+        //dpTable[i][sum] = result;
+        //lookup.put(key, result);
+        //return dpTable[i][sum];
+    }
+
     public void showResult() {
-        System.out.println("There is a feasible solution "  + dpTable[values.length][sum]);
-        if(!dpTable[values.length][sum]) {
+        System.out.println("There is a feasible solution " + dpTable[values.length][sum]);
+        if (!dpTable[values.length][sum]) {
             return;
         }
         int colIndex = this.sum;
         int rowIndex = this.values.length;
 
-        while(rowIndex > 0 || colIndex > 0) {
-            if(dpTable[rowIndex][colIndex] == dpTable[rowIndex - 1][colIndex]) {
-                rowIndex --;
+        while (rowIndex > 0 || colIndex > 0) {
+            if (dpTable[rowIndex][colIndex] == dpTable[rowIndex - 1][colIndex]) {
+                rowIndex--;
             } else {
                 System.out.println("We take the item : " + values[rowIndex - 1]);
                 colIndex = colIndex - values[rowIndex - 1];
-                rowIndex --;
+                rowIndex--;
             }
         }
     }
