9c370dffb545060b0e74261db5900ec503d20be6
Prerna <pregandhi@paypal.com>
2022-06-12 01:13:42 +0530

    Graph Algorithms : BFS

--- com/dsa/graph/algorithms/bfs/BreadthFirstSearch.java
+++ com/dsa/graph/algorithms/bfs/BreadthFirstSearch.java
@@ -9,33 +9,55 @@ import java.util.Queue;
 public class BreadthFirstSearch {
     // underlying data-structure used is queue
     // we visit neighbouring nodes and then add to the queue
-    // the item first added is visited first
-    //                     (A)
+    // the item first added in the queue is visited first
+    //                   (  A  )
     //               /      |       \
     //              /       |        \
     //             /        |         \
     //           (B)       (F)       (G)
-    //        /     \               /
-    //     (C)      (D)            (H)
-    //            /
-    //          (E)
+    //         /     \               /
+    //      (C)      (D)            (H)
+    //              /
+    //            (E)
     //
     // BFS with 'A' as source --> A B F G C D H E
+    // It is level order traversal for tree
+    // Time Complexity : O(V + E)
+
+    // visit        'A' add B, F, G to the queue
+    //  [B F G]
+    // visit        'B' add C, D to the queue
+    //  [F G C D]
+    // visit        'F'
+    //  [G C D]
+    // visit        'G' add H to the queue
+    //  [ C D H]
+    // visit        'C'
+    //  [D H]
+    // visit        'D' add E to the queue
+    //  [H E]
+    // visit        'H'
+    //  [E]
+    // visit        'E'
 
     public void traverse(Vertex root) {
 
         Queue<Vertex> queue = new LinkedList<>();
+        // visit the root node
         root.setVisited(true);
-
+        //add the root node to the queue
         queue.add(root);
 
-        while(!queue.isEmpty()) {
-            Vertex vertex = queue.remove();
+        while (!queue.isEmpty()) {
+            // remove the first element in the queue
+            Vertex vertex = queue.poll();
             System.out.println("Actual visited vertex : " + vertex);
 
             List<Vertex> neighbours = vertex.getNeighbours();
-            for(Vertex neighbourVertex : neighbours) {
-                if(!neighbourVertex.isVisited()) {
+            // visit neighbouring vertices
+            // add all neighbouring(adjacent vertices to the queue)
+            for (Vertex neighbourVertex : neighbours) {
+                if (!neighbourVertex.isVisited()) {
                     neighbourVertex.setVisited(true);
                     queue.add(neighbourVertex);
                 }
