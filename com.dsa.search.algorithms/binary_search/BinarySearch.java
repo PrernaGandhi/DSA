ea89ada152ee680484c278f409d67598cd064d53
Prerna <pregandhi@paypal.com>
2022-06-14 01:35:48 +0530

    Search Algorithms : Binary Search

--- com.dsa.search.algorithms/binary_search/BinarySearch.java
+++ com.dsa.search.algorithms/binary_search/BinarySearch.java
@@ -6,12 +6,12 @@ public class BinarySearch {
         int middle = arr.length / 2;
         int end = arr.length;
         int start = 0;
-        while(start < end) {
-            if(element == arr[middle]) return middle;
-            if(element < arr[middle]) {
+        while (start < end) {
+            if (element == arr[middle]) return middle;
+            if (element < arr[middle]) {
                 end = middle;
                 middle = (start + end) / 2;
-            } else if (element > arr[middle]){
+            } else if (element > arr[middle]) {
                 start = middle + 1;
                 middle = (start + end) / 2;
             }
@@ -20,24 +20,24 @@ public class BinarySearch {
     }
 
     public int recursiveHeadSearch(int[] arr, int element, int start, int middle, int end) {
-        if(start >= end) return -1;
-        if(arr[middle] == element)
+        if (start >= end) return -1;
+        if (arr[middle] == element)
             return middle;
-        if(element < arr[middle])
-            return recursiveHeadSearch(arr, element, start,(start + middle) / 2 ,middle);
+        if (element < arr[middle])
+            return recursiveHeadSearch(arr, element, start, (start + middle) / 2, middle);
         else
-            return recursiveHeadSearch(arr, element, middle + 1,(middle + 1 + end) / 2 ,end);
+            return recursiveHeadSearch(arr, element, middle + 1, (middle + 1 + end) / 2, end);
     }
 
     public int recursiveTailSearch(int[] arr, int element, int start, int middle, int end) {
-        if(start >= end) return -1;
-        if(arr[middle] == element) return middle;
-        if(element < arr[middle])
+        if (start >= end) return -1;
+        if (arr[middle] == element) return middle;
+        if (element < arr[middle])
             end = middle;
-         else
+        else
             start = middle + 1;
 
         middle = (start + end) / 2;
-        return recursiveTailSearch(arr, element, start,middle ,end);
+        return recursiveTailSearch(arr, element, start, middle, end);
     }
 }
