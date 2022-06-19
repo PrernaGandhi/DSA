f8fef0c9969e7fd1001787f449650d2cf67f81db
Prerna <pregandhi@paypal.com>
2022-06-19 23:47:47 +0530

    Recursion : Get the first index of element in an array

---
+++ com.dsa.recursion/intermediate/first_index_of_element/Runner.java
@@ -0,0 +1,10 @@
+package intermediate.first_index_of_element;
+
+public class Runner {
+    public static void main(String[] args) {
+        FirstIndexOfElement firstIndexOfElement = new FirstIndexOfElement();
+        int[] array = {1, 2, 3, 5, 4, 5, 5};
+        System.out.println(firstIndexOfElement.find(array, 0, 5));
+        System.out.println(firstIndexOfElement.find(array, 0, 2));
+    }
+}
