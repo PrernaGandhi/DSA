7f0e19295692c383271317715fb29c9bd63e2873
Prerna <pregandhi@paypal.com>
2022-06-19 15:31:58 +0530

    Recursion : Multiplication Of Two Numbers without using * operator

---
+++ com.dsa.recursion/multiplication_of_two_numbers/MultiplicationOfTwoNumbers.java
@@ -0,0 +1,20 @@
+package multiplication_of_two_numbers;
+
+/*
+    We need to multiply two numbers, but we can't use the multiply operator
+    We need to use the plus operator
+
+    m*n == m + m + m + ............. n times
+
+ */
+public class MultiplicationOfTwoNumbers {
+    public int calculate(int numberOne, int numberTwo) {
+        if (numberTwo == 0) {
+            return 0;
+        }
+
+        int result = calculate(numberOne, numberTwo - 1);
+
+        return numberOne + result;
+    }
+}
---
+++ com.dsa.recursion/multiplication_of_two_numbers/Runner.java
@@ -0,0 +1,10 @@
+package multiplication_of_two_numbers;
+
+public class Runner {
+    public static void main(String[] args) {
+        MultiplicationOfTwoNumbers multiplicationOfTwoNumbers = new MultiplicationOfTwoNumbers();
+        System.out.println(multiplicationOfTwoNumbers.calculate(4, 5));
+        System.out.println(multiplicationOfTwoNumbers.calculate(4, 10));
+        System.out.println(multiplicationOfTwoNumbers.calculate(4, 20));
+    }
+}
