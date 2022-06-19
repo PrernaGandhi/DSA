209b74ec1802a319d48a6c8aff7cb6f02b4147d9
Prerna <pregandhi@paypal.com>
2022-06-20 00:30:03 +0530

    Recursion : Last index of Element, modified as first index, just changed ordering

--- com.dsa.recursion/intermediate/last_index_of_element/LastIndexOfElement.java
+++ com.dsa.recursion/intermediate/last_index_of_element/LastIndexOfElement.java
@@ -2,13 +2,13 @@ package intermediate.last_index_of_element;
 
 public class LastIndexOfElement {
     public int find(int[] array, int current, int searchElement) {
-        if (current == -1) {
+        if (current == array.length) {
             return -1;
         }
+        int result = find(array, current + 1, searchElement);
         if (array[current] == searchElement) {
             return current;
         }
-        int result = find(array, current - 1, searchElement);
         return result;
     }
 }
--- com.dsa.recursion/intermediate/last_index_of_element/Runner.java
+++ com.dsa.recursion/intermediate/last_index_of_element/Runner.java
@@ -5,8 +5,8 @@ public class Runner {
     public static void main(String[] args) {
         LastIndexOfElement lastIndexOfElement = new LastIndexOfElement();
         int[] array = {1, 2, 3, 5, 4, 5, 5};
-        System.out.println(lastIndexOfElement.find(array, array.length - 1, 5));
-        System.out.println(lastIndexOfElement.find(array, array.length - 1, 2));
-        System.out.println(lastIndexOfElement.find(array, array.length - 1, 10));
+        System.out.println(lastIndexOfElement.find(array, 0, 5));
+        System.out.println(lastIndexOfElement.find(array, 0, 2));
+        System.out.println(lastIndexOfElement.find(array, 0, 10));
     }
 }
