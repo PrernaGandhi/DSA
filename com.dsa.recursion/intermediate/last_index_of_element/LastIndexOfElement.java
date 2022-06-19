6f524f4690b236eadb4556e59c1b9da468b1d58b
Prerna <pregandhi@paypal.com>
2022-06-19 23:48:02 +0530

    Recursion : Get the last index of element in an array

---
+++ com.dsa.recursion/intermediate/last_index_of_element/LastIndexOfElement.java
@@ -0,0 +1,14 @@
+package intermediate.last_index_of_element;
+
+public class LastIndexOfElement {
+    public int find(int[] array, int current, int searchElement) {
+        if (current == array.length) {
+            return -1;
+        }
+        if (array[current] == searchElement) {
+            return current;
+        }
+        int result = find(array, current - 1, searchElement);
+        return result;
+    }
+}
---
+++ com.dsa.recursion/intermediate/last_index_of_element/Runner.java
@@ -0,0 +1,11 @@
+package intermediate.last_index_of_element;
+
+
+public class Runner {
+    public static void main(String[] args) {
+        LastIndexOfElement lastIndexOfElement = new LastIndexOfElement();
+        int[] array = {1, 2, 3, 5, 4, 5, 5};
+        System.out.println(lastIndexOfElement.find(array, array.length - 1, 5));
+        System.out.println(lastIndexOfElement.find(array, array.length - 1, 2));
+    }
+}
