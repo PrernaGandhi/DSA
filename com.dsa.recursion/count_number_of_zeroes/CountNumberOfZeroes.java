a43c22d4766b68f7076d13e86f4a9541363ac55a
Prerna <pregandhi@paypal.com>
2022-06-19 15:38:44 +0530

    Recursion : Count Number of zeroes

---
+++ com.dsa.recursion/count_number_of_zeroes/CountNumberOfZeroes.java
@@ -0,0 +1,15 @@
+package count_number_of_zeroes;
+
+public class CountNumberOfZeroes {
+    public int calculate(int number) {
+        if (number == 0) {
+            return 0;
+        }
+
+        int result = calculate(number / 10);
+        if (number % 10 == 0) {
+            result = result + 1;
+        }
+        return result;
+    }
+}
---
+++ com.dsa.recursion/count_number_of_zeroes/Runner.java
@@ -0,0 +1,11 @@
+package count_number_of_zeroes;
+
+public class Runner {
+    public static void main(String[] args) {
+        CountNumberOfZeroes countNumberOfZeroes = new CountNumberOfZeroes();
+        System.out.println(countNumberOfZeroes.calculate(10320));
+        System.out.println(countNumberOfZeroes.calculate(1032000));
+        System.out.println(countNumberOfZeroes.calculate(1000000));
+        System.out.println(countNumberOfZeroes.calculate(10000001));
+    }
+}
