e9288909fdc51b61c3cdbf16e790a0324831819a
Prerna <pregandhi@paypal.com>
2022-06-19 15:44:05 +0530

    Recursion : Geometric Sum

---
+++ com.dsa.recursion/geometric_sum/GeometricSum.java
@@ -0,0 +1,13 @@
+package geometric_sum;
+
+public class GeometricSum {
+    public double calculate(int k) {
+        if (k == 0) {
+            return 1;
+        }
+
+        double result = calculate(k - 1);
+
+        return 1.0 / Math.pow(2, k) + result;
+    }
+}
---
+++ com.dsa.recursion/geometric_sum/Runner.java
@@ -0,0 +1,11 @@
+package geometric_sum;
+
+public class Runner {
+    public static void main(String[] args) {
+        GeometricSum geometricSum = new GeometricSum();
+        System.out.println(geometricSum.calculate(0));
+        System.out.println(geometricSum.calculate(1));
+        System.out.println(geometricSum.calculate(2));
+        System.out.println(geometricSum.calculate(3));
+    }
+}
