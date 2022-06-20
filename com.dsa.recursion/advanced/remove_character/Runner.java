3077306fe868b31925b352e7e578183091a0100e
Prerna <pregandhi@paypal.com>
2022-06-20 19:25:29 +0530

    Recursion : added two more test cases for Remove Character

--- com.dsa.recursion/advanced/remove_character/Runner.java
+++ com.dsa.recursion/advanced/remove_character/Runner.java
@@ -9,5 +9,9 @@ public class Runner {
         System.out.println(removeCharacter.remove(string, 'a', 0));
         string = "abcdsdaabef";
         System.out.println(removeCharacter.remove(string, 'b', 0));
+        string = "b";
+        System.out.println(removeCharacter.remove(string, 'b', 0));
+        string = "ba";
+        System.out.println(removeCharacter.remove(string, 'b', 0));
     }
 }
