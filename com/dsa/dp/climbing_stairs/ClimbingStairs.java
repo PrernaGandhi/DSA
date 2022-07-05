ebea1dea965fd71eca1380add927c1b16bbcbdc4
Prerna <pregandhi@paypal.com>
2022-07-05 11:08:22 +0530

    Dynamic programming : climbing stairs

--- com/dsa/dp/climbing_stairs/ClimbingStairs.java
+++ com/dsa/dp/climbing_stairs/ClimbingStairs.java
@@ -12,6 +12,26 @@ import java.util.Map;
         0 -> 2
         0 -> 2 -> 2     X
 
+        n = 5
+                       __
+                    __|  |
+                 __|  |  |
+              __|  |  |  |
+           __|  |  |  |  |
+        0 |  |  |  |  |  |
+            1  2  3  4  5
+
+    base case to reach 5 from 5, we have only 1 way
+    base case to reach 5 from 4, we have only 1 way
+
+    now to reach 5 from 3, we can either go to 4 by taking 1 step, or to 5 by taking 2 steps
+    but we already know the values for 4 and 5, 1 way[(3 to 4) to (4 to 5)], and 1 way [(3 to 5) to 1(5 to 5)]
+    so from 3, we can use values of 4 and 5 = 1 + 1 = 2,
+
+    now for 2, we can either reach 3 by taking 1 step, or 4 by taking 2 steps
+    now to calculate no of ways to reach 5 from 2 would be, value of 3 + value of 4 = 2 + 1 = 3
+
+
  */
 public class ClimbingStairs {
     private int countNumberOfWays(int n, int currentStep) {
