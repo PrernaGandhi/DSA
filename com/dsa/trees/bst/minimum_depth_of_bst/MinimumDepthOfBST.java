36e13fce2f46770e2aa3625bca8da9d90ad620a1
Prerna <pregandhi@paypal.com>
2022-07-08 22:52:20 +0530

    Trees : Minimum depth

--- com/dsa/trees/bst/minimum_depth_of_bst/MinimumDepthOfBST.java
+++ com/dsa/trees/bst/minimum_depth_of_bst/MinimumDepthOfBST.java
@@ -5,7 +5,10 @@ import com.dsa.trees.bst.Node;
 /*
         Minimum depth of a binary tree,
         - min distance from root node to leaf node
-
+        It is same as calculating height of BST,
+        except for finding the minimum depth,
+        we take the minimum of the two heights
+        of the sub trees and add 1 to it.
 
                     9
                    /  \
