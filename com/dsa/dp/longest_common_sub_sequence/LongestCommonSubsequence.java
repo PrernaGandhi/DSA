14ca54aa4d90d15d4ab0640dd72d0cf3546c8deb
Prerna <pregandhi@paypal.com>
2022-08-06 23:42:20 +0530

    [dp] : longest common subsequence problem

--- com/dsa/dp/longest_common_sub_sequence/LongestCommonSubsequence.java
+++ com/dsa/dp/longest_common_sub_sequence/LongestCommonSubsequence.java
@@ -1,5 +1,7 @@
 package com.dsa.dp.longest_common_sub_sequence;
 
+import java.util.Map;
+
 /*
         Example:
             Input 1: "apple"
@@ -16,32 +18,54 @@ package com.dsa.dp.longest_common_sub_sequence;
             Input 2: "maengo"
 
             Output: "ae"
+
+            Recurrence relation :
+
+                        0                               i=length of s1, or j=length of s2
+            lcs(i,j) =  1 + lcs(i+1,j+1)                when s[i]=s[j]
+                        max(lcs(i+1,j),lcs(i,j+1))      otherwise
+
+
  */
 public class LongestCommonSubsequence {
 
-    private int lcs(String str1, int i, String str2, int j) {
+    public int lcsUsingRecursion(String str1, int i, String str2, int j) {
         if (i == str1.length() || j == str2.length()) {
             return 0;
         }
 
         if (str1.charAt(i) == str2.charAt(j)) {
-            return 1 + lcs(str1, i + 1, str2, j + 1);
+            return 1 + lcsUsingRecursion(str1, i + 1, str2, j + 1);
         } else {
-            return Math.max(lcs(str1, i, str2, j + 1), lcs(str1, i + 1, str2, j));
+            return Math.max(lcsUsingRecursion(str1, i, str2, j + 1), lcsUsingRecursion(str1, i + 1, str2, j));
         }
     }
 
-    public int longestCommonSubsequence(String str1, String str2) {
-        return lcs(str1, 0, str2, 0);
+    public int lcsUsingRecursionUsingMemoization(String str1, int i, String str2, int j, Map<String, Integer> lookupTable) {
+        String key = i + ", " + j;
+        if (lookupTable.containsKey(key)) {
+            return lookupTable.get(key);
+        }
+        if (i == str1.length() || j == str2.length()) {
+            return 0;
+        }
+
+        int value;
+        if (str1.charAt(i) == str2.charAt(j)) {
+            value = 1 + lcsUsingRecursionUsingMemoization(str1, i + 1, str2, j + 1, lookupTable);
+        } else {
+            value = Math.max(lcsUsingRecursionUsingMemoization(str1, i, str2, j + 1, lookupTable),
+                    lcsUsingRecursionUsingMemoization(str1, i + 1, str2, j, lookupTable));
+        }
+        lookupTable.put(key, value);
+        return value;
     }
 
-    public int longestCommonSubsequenceDP(String str1, String str2) {
+    public int lcsUsingTabulation(String str1, String str2) {
         int[][] dp = new int[str1.length() + 1][str2.length() + 1];
-        for (int i = 0; i <= str1.length(); i++) {
-            for (int j = 0; j <= str2.length(); j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                } else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
                     dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
@@ -49,5 +73,21 @@ public class LongestCommonSubsequence {
             }
         }
         return dp[str1.length()][str2.length()];
+    }
+
+    public int lcsUsingOptimizedTabulation(String str1, String str2) {
+        int[] dp = new int[str2.length() + 1];
+        int[] prevDp = new int[str2.length() + 1];
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[j] = 1 + prevDp[j - 1];
+                } else {
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
+                }
+                prevDp = dp;
+            }
+        }
+        return dp[str2.length()];
     }
 }
--- com/dsa/dp/longest_common_sub_sequence/Runner.java
+++ com/dsa/dp/longest_common_sub_sequence/Runner.java
@@ -1,13 +1,25 @@
 package com.dsa.dp.longest_common_sub_sequence;
 
+import java.util.HashMap;
+
 public class Runner {
     public static void main(String[] args) {
         LongestCommonSubsequence lcs = new LongestCommonSubsequence();
-        System.out.println(lcs.longestCommonSubsequence("apple","mango"));
-        System.out.println(lcs.longestCommonSubsequence("apple","maengo"));
-        System.out.println(lcs.longestCommonSubsequence("apple","ample"));
-        System.out.println(lcs.longestCommonSubsequenceDP("apple","mango"));
-        System.out.println(lcs.longestCommonSubsequenceDP("apple","maengo"));
-        System.out.println(lcs.longestCommonSubsequenceDP("apple","ample"));
+        System.out.println("lcs using recursion : ");
+        System.out.println(lcs.lcsUsingRecursion("apple", 0, "mango", 0));
+        System.out.println(lcs.lcsUsingRecursion("apple", 0, "maengo", 0));
+        System.out.println(lcs.lcsUsingRecursion("apple", 0, "ample", 0));
+        System.out.println("lcs using memoization : ");
+        System.out.println(lcs.lcsUsingRecursionUsingMemoization("apple", 0, "mango", 0, new HashMap<>()));
+        System.out.println(lcs.lcsUsingRecursionUsingMemoization("apple", 0, "maengo", 0, new HashMap<>()));
+        System.out.println(lcs.lcsUsingRecursionUsingMemoization("apple", 0, "ample", 0, new HashMap<>()));
+        System.out.println("lcs using tabulation : ");
+        System.out.println(lcs.lcsUsingTabulation("apple", "mango"));
+        System.out.println(lcs.lcsUsingTabulation("apple", "maengo"));
+        System.out.println(lcs.lcsUsingTabulation("apple", "ample"));
+        System.out.println("lcs using optimized tabulation : ");
+        System.out.println(lcs.lcsUsingOptimizedTabulation("apple", "mango"));
+        System.out.println(lcs.lcsUsingOptimizedTabulation("apple", "maengo"));
+        System.out.println(lcs.lcsUsingOptimizedTabulation("apple", "ample"));
     }
 }
