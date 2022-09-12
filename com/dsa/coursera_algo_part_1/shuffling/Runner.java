339583c99fd6233b0806b81e384efc8983119675
Prerna <pregandhi@paypal.com>
2022-09-12 13:39:47 +0530

    [coursera][algorithms][part1] : Shuffling

---
+++ com/dsa/coursera_algo_part_1/shuffling/Runner.java
@@ -0,0 +1,17 @@
+package com.dsa.coursera_algo_part_1.shuffling;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class Runner {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Integer[] elements = {7, 10, 5, 3, 8, 4, 2, 9, 6};
+        Shuffling shuffling = new Shuffling();
+        Arrays.stream(elements).forEach(e ->System.out.print(e + " "));
+        shuffling.shuffle(elements);
+        System.out.println();
+        Arrays.stream(elements).forEach(e ->System.out.print(e + " "));
+    }
+
+}
---
+++ com/dsa/coursera_algo_part_1/shuffling/Shuffling.java
@@ -0,0 +1,30 @@
+package com.dsa.coursera_algo_part_1.shuffling;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+
+/*
+    We are shuffling the deck of cards, but generating a random number and swapping the cards at
+    current index and random index.
+    Note random number is generated between 0 th index and the current index (both inclusive)
+
+    Complexity : O(N)
+ */
+public class Shuffling {
+
+    private final Random random = SecureRandom.getInstanceStrong();
+
+    public Shuffling() throws NoSuchAlgorithmException {
+    }
+
+    void shuffle(Integer[] array){
+        for (int i = 0; i < array.length; i++) {
+            int num = random.nextInt(i+1);
+            int temp = array[i];
+            array[i] = array[num];
+            array[num] = temp;
+        }
+    }
+
+}
