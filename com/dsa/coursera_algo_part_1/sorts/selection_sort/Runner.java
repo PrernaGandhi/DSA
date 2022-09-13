2416afee714dcf85796277791672749d67a295b4
Prerna <pregandhi@paypal.com>
2022-09-13 07:10:34 +0530

    [coursera][algorithms][part1] : Adding test case values as comments for future reference

--- com/dsa/coursera_algo_part_1/sorts/selection_sort/Runner.java
+++ com/dsa/coursera_algo_part_1/sorts/selection_sort/Runner.java
@@ -8,6 +8,8 @@ public class Runner {
     public static void main(String[] args) {
         Sort<Integer> sort = new SelectionSort<>();
         Integer[] elements = {7, 10, 5, 3, 8, 4, 2, 9, 6};
+
+        // 2	3	4	5	6	7	8	9	10
         elements = sort.sort(elements);
         Arrays.stream(elements).forEach(e -> System.out.print(e + "\t"));
 
@@ -15,6 +17,8 @@ public class Runner {
 
         Sort<String> sortStrings = new SelectionSort<>();
         String[] strings = {"S","O","R","T","E","X","A","M","P","L","E"};
+
+        // A	E	E	L	M	O	P	R	S	T	X
         strings = sortStrings.sort(strings);
         Arrays.stream(strings).forEach(e -> System.out.print(e + "\t"));
 
